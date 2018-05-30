6bf3acc3391dfe19bd2256a36ab1cc7fb9d11a54
Jennifer Lieby <ze_nifer@hotmail.com>
2018-05-30 17:08:49 +0200

    ajout navbar + footer dans la vue base htl twig

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+
+$(document).ready(function () {
+    console.log("ok, JQuery fonctionne !");
+});
+
+console.log('Bienvenue sur WebPack!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,36 @@
-let Encore = require('@symfony/webpack-encore');
+var Encore = require('@symfony/webpack-encore');
 
 Encore
+// the project directory where all compiled assets will be stored
     .setOutputPath('web/build/')
+
+    // the public path used by the web server to access the previous directory
     .setPublicPath('/web')
-    .autoProvidejQuery()
+
+    // will create web/build/app.js and web/build/app.css
     .addEntry('app', './assets/js/app.js')
+
     .addEntry('style', './assets/scss/style.scss')
+    .addEntry('images', './assets/images/')
+
+    // allow legacy applications to use $/jQuery as a global variable
+    .autoProvidejQuery()
+
+    // enable source maps during development
+    .enableSourceMaps(!Encore.isProduction())
+
+    // empty the outputPath dir before each build
     .cleanupOutputBeforeBuild()
+
+    // show OS notifications when builds finish/fail
     .enableBuildNotifications()
-    .enableSassLoader(function(sassOptions) {}, {resolveUrlLoader: false })
+
+// create hashed filenames (e.g. app.abc123.css)
+    .enableVersioning()
+
+// allow sass/scss files to be processed
+ .enableSassLoader()
 ;
 
+// export the final configuration
 module.exports = Encore.getWebpackConfig();
