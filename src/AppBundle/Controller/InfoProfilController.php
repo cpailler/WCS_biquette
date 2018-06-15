be94359e8ed4bad57a4e72fc301bf08d5603c333
cpailler <aprel.illectemn@gmail.com>
2018-06-15 10:53:16 +0200

    stashing

--- src/AppBundle/Controller/InfoProfilController.php
+++ src/AppBundle/Controller/InfoProfilController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Genre;
 use AppBundle\Form\InfoProfilType;
 use AppBundle\Entity\Membre;
+use AppBundle\Form\NewPasswordType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -22,7 +23,7 @@ class InfoProfilController extends Controller
      * @Method({"GET", "POST"})
      *
      */
-    public function InfosConfidentiellesAction(Request $request)
+    public function InfosAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
         $membre=$this->getUser();
@@ -42,6 +43,32 @@ class InfoProfilController extends Controller
             'form'=>$form->createView(),
             'membre' => $membre,
             'genres' => $genres
+        ));
+    }
+
+    /**
+     * @Route("/password", name="profil_password")
+     * @Method({"GET", "POST"})
+     *
+     */
+
+    public function PasswordAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $membre=$this->getUser();
+
+        $form = $this->createForm(NewPasswordType::class, $membre);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($membre);
+            $em->flush();
+
+        }
+
+        return $this->render('profil/layoutProfil.html.twig', array(
+            'formulaire'=>'profil/passwordProfil.html.twig',
+            'form'=>$form->createView(),
+            'membre' => $membre
         ));
     }
 }
