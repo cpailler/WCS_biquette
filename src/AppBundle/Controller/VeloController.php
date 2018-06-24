c8aa73bf93997e30fa3c93662848ac137f43abe5
Hery Rasamimanana <hery.rasamimanana@gmail.com>
2018-06-25 00:45:38 +0200

    insert trier par distance/pts/typype

--- src/AppBundle/Controller/VeloController.php
+++ src/AppBundle/Controller/VeloController.php
@@ -183,7 +183,6 @@ class VeloController extends Controller
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
             $this->getDoctrine()->getManager()->flush();
         }
 
