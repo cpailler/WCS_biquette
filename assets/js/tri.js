2c51884679dc1a864637249b156733efcf62eb91
Hery Rasamimanana <hery.rasamimanana@gmail.com>
2018-07-01 20:01:49 +0200

    tri Pts par palier

--- assets/js/tri.js
+++ assets/js/tri.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 
             $(this).parent().show();
 
-            if($( this ).val() != selectedValue)
+            if($(this).val() != selectedValue)
             {
                 console.log($( this ).val());
                 $(this).parent().hide();
@@ -19,3 +19,37 @@ $(document).ready(function(){
     });
 });
 
+
+$(document).ready(function(){
+    $('.points').change(function(){
+        var selectedValue = $(this).val();
+
+        console.log(selectedValue);
+
+        $('.pts-velo').each(function() {
+
+            $(this).parent().show();
+
+            if(($(this).val() > 249) && (selectedValue == 'area1')) {
+                console.log($( this ).val());
+                $(this).parent().hide();
+            } else if(($(this).val() < 250) && ($(this).val() > 499) && (selectedValue == 'area2'))
+            {
+                console.log($( this ).val());
+                $(this).parent().hide();
+            } else if(($(this).val() < 500) && ($(this).val() > 749) && (selectedValue == 'area3'))
+            {
+                console.log($( this ).val());
+                $(this).parent().hide();
+            } else if(($(this).val() < 750) && ($(this).val() > 999) && (selectedValue == 'area4'))
+            {
+                console.log($( this ).val());
+                $(this).parent().hide();
+            } else if(($(this).val() < 1000) && (selectedValue == 'area5'))
+            {
+                console.log($( this ).val());
+                $(this).parent().hide();
+            }
+        });
+    });
+});
