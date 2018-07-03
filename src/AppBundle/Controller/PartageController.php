e4ef0095c3d7c961af3bda3144e7b2b70458c251
SIDELNIKOV Ilya <sidelnikov.ilya@gmail.com>
2018-07-03 10:54:24 +0200

    partage ver 2

--- src/AppBundle/Controller/PartageController.php
+++ src/AppBundle/Controller/PartageController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Reservation;
 use AppBundle\Entity\Velo;
+use AppBundle\Form\ReservationType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -21,13 +22,31 @@ class PartageController extends Controller
      * @Route("/{id}/reservation", name="partage_reservation")
      *
      */
-    public function utilisateurReservationAction(Request $request,Velo $velo)
+    public function utilisateurReservationAction(Request $request, Velo $velo)
     {
         $membre = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $reservation = New Reservation();
+        $reservationForm = $this->createForm(ReservationType::class, $reservation);
+        $reservationForm->handleRequest($request);
+        if ($reservationForm->isSubmitted() && $reservationForm->isValid()) {
+            
+            $reservation->setVelo($velo->getId());
+            $reservation->setLocataire($membre);
+            $reservation->setCoutPts($velo->getCoutPts());
+            $reservation->setCaution($velo->getCaution());
+            $reservation->setAssurance($velo->getAssurOblig());
+
+
+
+            $em->persist($velo);
+            $em->flush();
+        }
 
 
         return $this->render('partage/utilisateur_reservation.html.twig',array(
             'membre' => $membre,
+            'reservationForm' => $reservationForm->createView(),
             'velo' => $velo));
     }
 
