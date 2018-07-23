b2a0ccbe2df56582e3d92f462a4d33600836f18a
SIDELNIKOV Ilya <sidelnikov.ilya@gmail.com>
2018-07-23 17:37:21 +0200

    version2

--- TEMP_MANGOPAY/MangoPaySdkStorage.tmp.php
+++ TEMP_MANGOPAY/MangoPaySdkStorage.tmp.php
@@ -1,1 +1,1 @@
-<?php exit(); ?>O:29:"MangoPay\Libraries\OAuthToken":5:{s:11:"create_time";i:1532087760;s:12:"access_token";s:32:"48b1309c9f1f4f9e8aae9b4bd3fe5673";s:10:"token_type";s:6:"bearer";s:10:"expires_in";i:1199;s:17:"autentication_key";s:32:"eef08411730ca46933ac904cf5785519";}
+<?php exit(); ?>O:29:"MangoPay\Libraries\OAuthToken":5:{s:11:"create_time";i:1532360165;s:12:"access_token";s:32:"7ee7f919463240ed89bf4d4b478c43e3";s:10:"token_type";s:6:"bearer";s:10:"expires_in";i:1199;s:17:"autentication_key";s:32:"eef08411730ca46933ac904cf5785519";}
